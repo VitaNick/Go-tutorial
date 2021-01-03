930f5df203561ca1aeda749d7503d3ad5b92c93c
Nick Vanden Eynde <nick@vitanick.dev>
2021-01-03 23:31:31 +0100

    changing the state of the struct

--- Creating custom data types with Go/src/main.go
+++ Creating custom data types with Go/src/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "organization"
+import (
+	"fmt"
+	"organization"
+)
 
 func main() {
 	p := organization.NewPerson("Collin", "Wilson")
+	err := p.SetTwitterHandler("@jam_wils")
+
+	if err != nil {
+		fmt.Printf("An error occured setting twitter handler: %s\n", err.Error())
+	}
 
 	println(p.ID())
 	println(p.FullName())
