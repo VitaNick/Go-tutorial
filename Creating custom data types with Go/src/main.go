c66394f61a2496f71405ecd01d8cba9181984ac5
Nick Vanden Eynde <nick@vitanick.dev>
2021-01-09 22:25:14 +0100

    Comparing structs

--- Creating custom data types with Go/src/main.go
+++ Creating custom data types with Go/src/main.go
@@ -1,25 +1,37 @@
 package main
 
-import (
-	"fmt"
-	"organization"
-)
-
 func main() {
-	p := organization.NewPerson("Nick", "Vanden Eynde", organization.NewEuropeanUnionIdentifier("123-45-6789", "Belgium"))
-	err := p.SetTwitterHandler(organization.TwitterHandler("@ninjawulf98"))
-	fmt.Printf("%T\n", organization.TwitterHandler("test"))
+	// p := organization.NewPerson("Nick", "Vanden Eynde", organization.NewEuropeanUnionIdentifier("123-45-6789", "Belgium"))
+	// err := p.SetTwitterHandler(organization.TwitterHandler("@ninjawulf98"))
+	// fmt.Printf("%T\n", organization.TwitterHandler("test"))
 
-	if err != nil {
-		fmt.Printf("An error occured setting twitter handler: %s\n", err.Error())
+	// if err != nil {
+	// 	fmt.Printf("An error occured setting twitter handler: %s\n", err.Error())
+	// }
+
+	// p.First = "Collin"
+	// println(p.First)
+	// println(p.Name.First)
+	// println(p.TwitterHandler())
+	// println(p.TwitterHandler().RedirectUrl())
+	// println(p.ID())
+	// println(p.Country())
+	// println(p.FullName())
+
+	name1 := Name{First: "James", Last: "Wilson"}
+	name2 := OtherName{First: "James", Last: "Wilson"}
+
+	if name1 == name2 {
+		println("We match")
 	}
+}
 
-	p.First = "Collin"
-	println(p.First)
-	println(p.Name.First)
-	println(p.TwitterHandler())
-	println(p.TwitterHandler().RedirectUrl())
-	println(p.ID())
-	println(p.Country())
-	println(p.FullName())
+type Name struct {
+	First string
+	Last  string
+}
+
+type OtherName struct {
+	First string
+	Last  string
 }
