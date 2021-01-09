8078e718ec18bf1d50a380be4803fb5c8a8f3bc3
Nick Vanden Eynde <nick@vitanick.dev>
2021-01-09 22:42:26 +0100

    Keys for maps

--- Creating custom data types with Go/src/main.go
+++ Creating custom data types with Go/src/main.go
@@ -1,13 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"organization"
+)
+
 func main() {
-	// p := organization.NewPerson("Nick", "Vanden Eynde", organization.NewEuropeanUnionIdentifier("123-45-6789", "Belgium"))
-	// err := p.SetTwitterHandler(organization.TwitterHandler("@ninjawulf98"))
+	p := organization.NewPerson("Nick", "Vanden Eynde", organization.NewEuropeanUnionIdentifier("123-45-6789", "Belgium"))
+	err := p.SetTwitterHandler(organization.TwitterHandler("@ninjawulf98"))
 	// fmt.Printf("%T\n", organization.TwitterHandler("test"))
 
-	// if err != nil {
-	// 	fmt.Printf("An error occured setting twitter handler: %s\n", err.Error())
-	// }
+	if err != nil {
+		fmt.Printf("An error occured setting twitter handler: %s\n", err.Error())
+	}
 
 	// p.First = "Collin"
 	// println(p.First)
@@ -18,16 +23,18 @@ func main() {
 	// println(p.Country())
 	// println(p.FullName())
 
-	// ssn := organization.NewSocialSecurityNumber("123-45-6789")
-	// eu := organization.NewEuropeanUnionIdentifier("123-45-6789", "Belgium")
-	// eu2 := organization.NewEuropeanUnionIdentifier("123-45-6789", "Belgium")
-
 	name1 := Name{First: "", Last: ""}
 	// name2 := OtherName{First: "James", Last: "Wilson"}
 
-	if name1 == (Name{}) {
-		println("We match")
-	}
+	// ssn := organization.NewSocialSecurityNumber("123-45-6789")
+	// eu := organization.NewEuropeanUnionIdentifier("123-45-6789", "Belgium")
+	// eu2 := organization.NewEuropeanUnionIdentifier("123-45-6789", "Belgium")
+	portfolio := map[Name][]organization.Person{}
+	portfolio[name1] = []organization.Person{p}
+
+	// if name1 == (Name{}) {
+	// 	println("We match")
+	// }
 
 	// fmt.Printf("%T\n", ssn)
 	// fmt.Printf("%T\n", eu)
@@ -35,8 +42,9 @@ func main() {
 }
 
 type Name struct {
-	First string
-	Last  string
+	First  string
+	Last   string
+	Middle []string
 }
 
 type OtherName struct {
