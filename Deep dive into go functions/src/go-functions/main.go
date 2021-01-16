2b5c4a3155e066ed1a86ba9a420f2cd5da745164
Nick Vanden Eynde <nick@vitanick.dev>
2021-01-16 21:45:16 +0100

    Interfaces and receivers

--- Deep dive into go functions/src/go-functions/main.go
+++ Deep dive into go functions/src/go-functions/main.go
@@ -1,12 +1,30 @@
 package main
 
-import "go-functions/simplemath"
+import (
+	"net/http"
+	"strings"
+)
 
 func main() {
-	sv := simplemath.NewSemanticVersion(1, 2, 3)
-	sv.IncrementMajor()
-	println((sv.String()))
+	var tripper http.RoundTripper = &RoundTripCounter{}
+	r, _ := http.NewRequest(http.MethodGet, "http://pluralsight.com", strings.NewReader("test call"))
+	_, _ = tripper.RoundTrip(r)
 }
+
+type RoundTripCounter struct {
+	count int
+}
+
+func (rt *RoundTripCounter) RoundTrip(*http.Request) (*http.Response, error) {
+	rt.count += 1
+	return nil, nil
+}
+
+// func main() {
+// 	sv := simplemath.NewSemanticVersion(1, 2, 3)
+// 	sv.IncrementMajor()
+// 	println((sv.String()))
+// }
 
 // func main() {
 // 	answer, err := simplemath.Divide(6, 2)
