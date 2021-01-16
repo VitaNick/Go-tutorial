82cd394c75f3f6eddcc85eaf667cce399e23bbfc
Nick Vanden Eynde <nick@vitanick.dev>
2021-01-16 21:33:37 +0100

    method declarations

--- Deep dive into go functions/src/go-functions/main.go
+++ Deep dive into go functions/src/go-functions/main.go
@@ -1,19 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"go-functions/simplemath"
-)
+import "go-functions/simplemath"
 
 func main() {
-	answer, err := simplemath.Divide(6, 2)
-	if err != nil {
-		fmt.Printf("An error occured %s\n", err.Error())
-	} else {
-		fmt.Printf("%f\n", answer)
-	}
-
-	numbers := []float64{12.2, 14, 16, 22, 4}
-	total := simplemath.Sum(numbers...)
-	fmt.Printf("total of sum: %f\n", total)
+	sv := simplemath.NewSemanticVersion(1, 2, 3)
+	println((sv.String()))
 }
+
+// func main() {
+// 	answer, err := simplemath.Divide(6, 2)
+// 	if err != nil {
+// 		fmt.Printf("An error occured %s\n", err.Error())
+// 	} else {
+// 		fmt.Printf("%f\n", answer)
+// 	}
+
+// 	numbers := []float64{12.2, 14, 16, 22, 4}
+// 	total := simplemath.Sum(numbers...)
+// 	fmt.Printf("total of sum: %f\n", total)
+// }
