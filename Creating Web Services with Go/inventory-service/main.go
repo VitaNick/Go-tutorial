5f3d2f3b07bb88aea36f9e078b57122ab0b6ef6c
Nick Vanden Eynde <nick@vitanick.dev>
2021-01-17 21:30:06 +0100

    connecting to a database

--- Creating Web Services with Go/inventory-service/main.go
+++ Creating Web Services with Go/inventory-service/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/pluralsight/webservices/database"
 	"github.com/pluralsight/webservices/product"
 )
 
 const apiBasePath = "api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(apiBasePath)
 	http.ListenAndServe("localhost:5000", nil)
 }
