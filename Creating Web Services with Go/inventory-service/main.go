54027e70c4a96b9c42294c1043619659c9ee6587
Nick Vanden Eynde <nick@vitanick.dev>
2021-01-17 11:09:48 +0100

    project setup + creating handlers

---
+++ Creating Web Services with Go/inventory-service/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+type fooHandler struct {
+	Message string
+}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(f.Message))
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("bar called"))
+}
+
+func main() {
+	http.Handle("/foo", &fooHandler{Message: "Foo called"})
+	http.HandleFunc("/bar", barHandler)
+	http.ListenAndServe("localhost:5000", nil)
+}
