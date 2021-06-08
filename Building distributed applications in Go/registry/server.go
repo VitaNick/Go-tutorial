7548957d3a7024a0128768c52962f615fc9a12c9
Nick Vanden Eynde <nick@vitanick.dev>
2021-06-08 11:59:44 +0200

    Notifying when a dependency is started

--- Building distributed applications in Go/registry/server.go
+++ Building distributed applications in Go/registry/server.go
@@ -27,7 +27,45 @@ func (r *registry) add(reg Registration) error {
 		return err
 	}
 
+	r.notify(patch{
+		Added: []patchEntry{
+			patchEntry{Name: reg.ServiceName, URL: reg.ServiceURL},
+		},
+	})
 	return nil
+}
+
+func (r registry) notify(fullPatch patch) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
+	for _, reg := range r.registrations {
+		go func(reg Registration) {
+			for _, reqService := range reg.RequiredServices {
+				p := patch{Added: []patchEntry{}, Removed: []patchEntry{}}
+				sendUpdate := false
+				for _, added := range fullPatch.Added {
+					if added.Name == reqService {
+						p.Added = append(p.Added, added)
+						sendUpdate = true
+					}
+				}
+				for _, removed := range fullPatch.Removed {
+					if removed.Name == reqService {
+						p.Removed = append(p.Removed, removed)
+						sendUpdate = true
+					}
+				}
+				if sendUpdate {
+					err := r.sendPatch(p, reg.ServiceUpdateURL)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+				}
+			}
+		}(reg)
+	}
 }
 
 func (r registry) sendRequiredServices(reg Registration) error {
